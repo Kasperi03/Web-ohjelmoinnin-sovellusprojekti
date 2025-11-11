f6f5b8dd7b4c71a83fe407382820e2a58b1645f4
villepekkala <vpekkala04@gmail.com>
2025-11-12 01:24:58 +0200

    Grouplisting sivua tehty ja component listan jäsenelle

--- docker_fullstack/frontend/src/pages/groupList.jsx
+++ docker_fullstack/frontend/src/pages/groupList.jsx
@@ -1,10 +1,15 @@
 import "./styles/groupList.css";
+import GroupListing from "../components/groupListing.jsx";
 
 export default function GroupList() {
   return (
     <div className="groupList-container">
       <h1>Groups Page</h1>
       <p>This is where you can view groups.</p>
+
+      <button className="create-btn">Create New Group</button>
+
+      <GroupListing  name="test group" memberCount= "5"/>
     </div>
   );
 }
